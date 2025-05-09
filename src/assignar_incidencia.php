ce358ce10f2ece55037c6631d6a42cfb04170934
ricsaagon <a21ricsaagon@inspedralbes.cat>
2025-05-09 10:40:07 +0200

    Update assignar_incidencia.php

--- src/assignar_incidencia.php
+++ src/assignar_incidencia.php
@@ -6,17 +6,23 @@ $id = $_GET['id'] ?? null;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $empleat = $_POST['empleat'];
+    $prioritat = $_POST['Prioritat'];
 
-    // Assignar el tècnic (empleat) a la incidència
+
+    // Assignar el tècnici PRioritat a la incidència
     $conn->query("UPDATE Incidencies SET Empleat = '$empleat' WHERE ID = $id");
+    $conn->query("UPDATE Incidencies SET Prioritat = '$prioritat' WHERE ID = $id");
+
 
     echo "<p>Incidència assignada correctament!</p>";
     echo '<a href="incidencies_no_assignades.php">Tornar al llistat</a>';
     exit;
 }
 
-// Llistar empleats disponibles
+// Llistar empleats  y Prioritats 
 $empleats = $conn->query("SELECT DNI, Nom FROM Empleat");
+$Prioritats = $conn->query("SELECT ID, Nivel_de_Prioritat FROM Prioritat");
+
 ?>
 
 <!DOCTYPE html>
@@ -41,17 +47,23 @@ $empleats = $conn->query("SELECT DNI, Nom FROM Empleat");
 <fieldset>
 
 <form method="POST">
-<h1>Assignar Tècnic a la Incidència <?= htmlspecialchars($id) ?></h1>
+<h1>Assignar Tècnic/Prioritat a la Incidència <?= htmlspecialchars($id) ?></h1>
 
     <label for="empleat">Selecciona un tècnic:</label>
     <select name="empleat" id="empleat">
         <option value="">Selecciona un tècnic</option>
-        <?php while ($row = $empleats->fetch_assoc()): ?>
-            <option value="<?= $row['DNI'] ?>"><?= $row['Nom'] ?></option>
+        <?php while ($emp = $empleats->fetch_assoc()): ?>
+            <option value="<?= $emp['DNI'] ?>"><?= $emp['Nom'] ?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="Prioritat">Selecciona una Prioritat:</label>
+    <select name="Prioritat" id="Prioritat">
+        <option value="">Selecciona una Prioritat</option>
+        <?php while ($pri = $Prioritats->fetch_assoc()): ?>
+            <option value="<?= $pri['ID'] ?>"><?= $pri['Nivel_de_Prioritat'] ?></option>
         <?php endwhile; ?>
     </select>
     <br><br>
-
     
 
     <button type="submit">Assignar</button>
