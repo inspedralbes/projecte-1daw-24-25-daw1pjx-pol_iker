9466474fcfa43dfeb1705ff86da1b0d641ee8d92
IkerMata <a24ikematgar@inspedralbes.cat>
2025-05-09 10:26:58 +0200

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -19,24 +19,35 @@
     <div class="container mt-5 d-flex justify-content-center">
         <div class="row g-4" style="max-width: 1000px; margin-top: 60px;"> 
 
-          <div class="col-md-6">
+          <div class="col-md-4">
             <a href="crear.php">
             <div class="card text-center">
               <div class="card-body">
-                <img src="formulario.png" width="375px" style="margin-left: 35px;"  class="mb-3" alt="Formulario">
+                <img src="formulario.png" width="265px" style="margin-left: 35px;"  class="mb-3" alt="Formulario">
                 <h5 class="card-title">FORMULARI DE INICIDÈNCIES</h5>
                 <p class="card-text">Aquí podràs posar la teva incidència informàtica.</p>
               </div>
             </div>
             </a>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-4">
               <a href="llista.php">
             <div class="card text-center">
               <div class="card-body">
-                <img src="llista2.png" width="375px" class="mx-auto d-block mb-3" alt="Llista">
+                <img src="llista2.png" width="265px" class="mx-auto d-block mb-3" alt="Llista">
                 <h5 class="card-title">LLISTA DE TOTES LES INCIDÈNCIES</h5>
                 <p class="card-text">Aquí podràs veure totes les incidències fetes.</p>
+              </div>
+            </div>
+            </a>
+          </div>
+          <div class="col-md-4">
+              <a href="asignar.php">
+            <div class="card text-center">
+              <div class="card-body">
+                <img src="modificar.png" width="265px" class="mx-auto d-block mb-3" alt="Llista">
+                <h5 class="card-title">LLISTA DE INICIDÈNCIES NO FETES</h5>
+                <p class="card-text">Aquí podràs veure les incidencias no fetes.</p>
               </div>
             </div>
             </a>
